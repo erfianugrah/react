e9743b3cb590497cd7d52a8f2c523874ec27f24c
erfianugrah <erfi.anugrah@gmail.com>
2020-05-21 10:47:35 +0800

    Day 21/05/2020 - 10:47

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,33 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);a;
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = 
+var template = (
 <div>
     <h1>Indecision App</h1>
-        <p>This is some info</p>
-</div>;
-
+    <p>This is some info</p>
+    <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+    </ol>
+</div>
+);
 var appRoot = document.getElementById('app');
+
 ReactDOM.render(template, appRoot);
