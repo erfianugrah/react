b5ea469ffc28d66ba4b8098736018930f958c3ed
erfianugrah <erfi.anugrah@gmail.com>
2020-05-23 11:58:36 +0800

    renderTemplate 11:58

--- src/app.js
+++ src/app.js
@@ -1,47 +1,45 @@
-// JSX - JavaScript XML
 const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    options: ['One','Two']
+    options: []
 };
+const onFormSubmit = (e) => {
+    e.preventDefault();
 
+    const option = e.target.elements.option.value;
 
-const template = (
-<div>
-    <h1>{app.title ? app.title : 'It doesn\'t exist'}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
-    <ol>
-        <li>Item One</li>
-        <li>Item Two</li>
-    </ol>
-</div>
-);
-
-let count = 0;
-const addOne = () => {
-    console.log('addOne');
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = ' ';
+    }
+    renderTemplate();
 };
-const minusOne = () => {
-    console.log('minusOne');
-};
-const reset = () => {
-    console.log('reset');
-}
-
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>Reset</button>
-    </div>
-);
-
-// Challenge
-// Make Button "-1"- setup minusOne and register log "minusOne"
-// Make reset button "reset" -setup setup reset function log "reset"
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const renderTemplate () => {
+    const template = (
+        <div>
+            <h1>{app.title ? app.title : 'It doesn\'t exist'}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>Item One</li>
+                <li>Item Two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add Option</button>
+            </form>
+        </div>
+        );
+        ReactDOM.render(template, appRoot);       
+};
+
+renderTemplate();
+
+
+// Create Render Function that renders the new JSX
+// Call it right away
+// Call it after options array added to
