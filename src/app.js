1b0c2f57a0b59fde0abea210b965f62f1d3d3c1f
erfianugrah <erfi.anugrah@gmail.com>
2020-05-26 12:02:35 +0800

    Webpack with Babel

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
-// import './utils.js';
-import isSenior, {isAdult, canDrink} from './person.js';
-console.log(isAdult(21));
-console.log(canDrink(18));
-console.log(isSenior(65));
+// Install  -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import substract, {square, add} from './utils.js';
-// console.log('app.js is running');
-// console.log(square(3));
-// console.log(add(5,6));
-// console.log(substract(45,252));
+const template = <p>Hiya</p>;
+ReactDOM.render(template, document.getElementById('app'));
 
-// Setting the default export and function
-// Grab the default and call it
+// Loader
