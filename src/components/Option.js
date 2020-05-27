904eb8da32f7a4bf722fca935940ee2d89e4ba4d
erfianugrah <erfi.anugrah@gmail.com>
2020-05-27 08:57:41 +0800

    refactoring other stateless functional components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
+const Option = (props) => (
         <div>
             Option: {props.optionText}
             <button 
@@ -12,7 +11,6 @@ const Option = (props) => {
                 Remove
             </button>
         </div>
-    );
-};
+);
 
 export default Option;
