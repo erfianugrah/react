2d95437cd6d927f98115933d540479438ae4410a
erfianugrah <erfi.anugrah@gmail.com>
2020-05-30 14:46:13 +0800

    Router Portfolio Build it v2

--- portfolio-router/src/routers/AppRouter.js
+++ portfolio-router/src/routers/AppRouter.js
@@ -13,7 +13,7 @@ const AppRouter = () => (
             <Header />
             <Switch>
                 <Route path="/" component={HomePage} exact={true} />
-                <Route path="/portfolio" component={PortfolioPage} />
+                <Route path="/portfolio" component={PortfolioPage} exact={true} />
                 <Route path="/portfolio/:id" component={PortfolioItemPage} />
                 <Route path="/contact" component={ContactPage} />
                 <Route component={NotFoundPage} />
