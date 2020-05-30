384929ee254b6dc2c6ac8faffabca0189829c7df
erfianugrah <erfi.anugrah@gmail.com>
2020-05-30 12:52:26 +0800

    Query Strings with Parameters

--- router/src/components/Header.js
+++ router/src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Go home betch</NavLink>
         <NavLink to="/create" activeClassName="is-active">Go create betch</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Go edit betch</NavLink>
         <NavLink to="/help" activeClassName="is-active">Go help betch</NavLink>
     </header>
 );
