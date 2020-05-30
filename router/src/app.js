da3962e355cf27b0b4de1412e2391266d5441f12
erfianugrah <erfi.anugrah@gmail.com>
2020-05-30 12:17:21 +0800

    Linking between routes

--- router/src/app.js
+++ router/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import  './styles/styles.scss';
 
@@ -30,19 +30,32 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404! 
+        404! - <Link to="/">Go home betch</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Go home betch</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Go create betch</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Go edit betch</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Go help betch</NavLink>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
-        <Switch>
+        <div>
+            <Header />
+            <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
             <Route component={NotFoundPage} />
         </Switch>
+        </div>
     </BrowserRouter>
 );
 
