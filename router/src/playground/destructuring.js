1f42dd34dae61e9b094ca37122c29eacdd371f6e
erfianugrah <erfi.anugrah@gmail.com>
2020-05-31 09:42:28 +0800

    ES6 Array Destructuring

--- router/src/playground/destructuring.js
+++ router/src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// Object Destructuring
+
 // const person = {
 //     name: 'Erfi',
 //     age: 26,
@@ -12,14 +14,32 @@
 
 // console.log(`${name} is ${age} and is living in ${city} and it is ${temperature} right now`);
 
-const book = {
-    title: 'Dune',
-    author: 'Frank Herbert',
-    publisher: {
-        name: 'Penguin'
-    }
-};
-const {title, author} = book;
-const {name: publisherName = 'Self-published'} = book.publisher;
+// const book = {
+//     title: 'Dune',
+//     author: 'Frank Herbert',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
+// const {title, author} = book;
+// const {name: publisherName = 'Self-published'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+// Array Destructuring
+
+// const address = [
+//     '934 Jurong West Street 91',
+//     'Singapore',
+//     'Nanyang',
+//     '640934'
+// ];
+
+// const [, , state = 'Bishan'] = address;
+
+// console.log(`You are in  ${state}`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [itemName, ,mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
