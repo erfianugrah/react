89ee2f2487efda8ec370796c7e014b561db16ebf
erfianugrah <erfi.anugrah@gmail.com>
2020-05-31 13:33:30 +0800

    ES6 Spread Operators in Reducers

--- router/src/playground/redux-expensify.js
+++ router/src/playground/redux-expensify.js
@@ -1,7 +1,32 @@
 import { createStore, combineReducers } from 'redux';
+import {v4 as uuid} from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+    {
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = {}
+  ) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: uuid(),
+      description,
+      note,
+      amount,
+      createdAt
+    }
+  });
+
 // REMOVE_EXPENSE
+
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+  });
+
 // EDIT_EXPENSE
 // SET_TEXT_FIlTER
 // SORT_BY_DATE
@@ -12,12 +37,20 @@ import { createStore, combineReducers } from 'redux';
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
         default:
             return state;
     }
 };
+
+
 // Filters Reducer
-// text: '', sortBy => 'date', startDate => undefined, endDate => undefined
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
@@ -39,6 +72,15 @@ const store = createStore(
             filters: filtersReducer
         })
     );
+    
+store.subscribe (() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 400}));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 console.log(store.getState());
 
