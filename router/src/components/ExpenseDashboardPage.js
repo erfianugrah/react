1706b646fa67bb68bd507797123f83b2591ed65b
erfianugrah <erfi.anugrah@gmail.com>
2020-06-04 08:02:35 +0800

    Controlled Inputs for Filters

--- router/src/components/ExpenseDashboardPage.js
+++ router/src/components/ExpenseDashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilter';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
