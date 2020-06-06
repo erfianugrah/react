fc073f5a1889eabae702bf133c87b0b60b1ced13
erfianugrah <erfi.anugrah@gmail.com>
2020-06-06 11:51:43 +0800

    Setting up date picker

--- router/src/components/ExpenseForm.js
+++ router/src/components/ExpenseForm.js
@@ -1,25 +1,46 @@
 import React from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('Do MMM YYYY'));
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     };
+
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({ description }));
     };
+
     onNoteChange = (e) => {
         const note = e.target.value;
         this.setState(() => ({ note }));
     };
+
     onAmountChange = (e) => {
         const amount = e.target.value;
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
     };
+
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    };
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }));
+    };
+
     render() {
         return (
             <div>
@@ -36,6 +57,14 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={2}
+                        isOutsideRange={() => false}
                     />
                     <textarea 
                         placeholder="Add a note for your expense (optional)"
