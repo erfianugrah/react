532080dfab89ea0ff36719f5ab21edf849f68aa2
erfianugrah <erfi.anugrah@gmail.com>
2020-06-06 10:34:29 +0800

    Creating Expense Add/Edit Form

--- router/src/components/Header.js
+++ router/src/components/Header.js
@@ -4,9 +4,9 @@ import {NavLink} from 'react-router-dom';
 const Header = () => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Go home betch</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Go create betch</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Go help betch</NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 );
 
