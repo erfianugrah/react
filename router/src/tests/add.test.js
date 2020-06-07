6d6189bfeb9c1dc56bc804d0b13841228fd0234f
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-06-07 10:29:57 -0700

    Setting up jest

---
+++ router/src/tests/add.test.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b + 1;
+
+test('should add two numbers', () => {
+    const result = add(3, 4);
+
+    if (result !== 7) {
+    throw new Error(`You added 4 and 3. The reuslt was ${result}. Expect 7`)
+    }
+});
