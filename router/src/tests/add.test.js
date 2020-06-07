c66889595157faaed6f6903f7a1140cf3a346dfd
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-06-08 02:23:49 +0800

    Setting up jest v2

--- router/src/tests/add.test.js
+++ router/src/tests/add.test.js
@@ -1,6 +1,18 @@
-const add = (a, b) => a + b + 1;
+const add = (a, b) => a + b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
+
 
 test('should add two numbers', () => {
     const result = add(3, 4);
     expect(result).toBe(7);
+});
+
+test('should be erfi', () => {
+    const result = generateGreeting('Erfi');
+    expect(result).toBe('Hello Erfi!');
+});
+
+test("should generate greeting for no name", () => {
+  const result = generateGreeting();
+  expect(result).toBe('Hello Anonymous!');
 });
