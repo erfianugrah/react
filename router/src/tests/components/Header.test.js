2f9a36e7a5e69f737581f591a80e1dca12c09298
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-06-10 07:18:03 +0800

    Finally setup Enzyme

--- router/src/tests/components/Header.test.js
+++ router/src/tests/components/Header.test.js
@@ -3,8 +3,11 @@ import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
-
 });
