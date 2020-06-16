dce916f45ecfb3168d99fd963e43c407769a8f24
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-06-16 14:01:29 +0800

    Creating Separate CSS Files

--- router/webpack.config.js
+++ router/webpack.config.js
@@ -1,30 +1,49 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const loader = require('sass-loader');
 
-module.exports = {
-    entry: './src/app.js',
-    mode: 'development',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },  
-        {
-            test: /\.s?css$/g,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        mode: 'development',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },  
+            {
+                test: /\.s?css$/g,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: css-loader,
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin('main.css')
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
