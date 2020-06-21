301292e76fe7222692c584de177543af00ad1573
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-06-21 10:43:38 +0800

    Expenses Sumamry

--- router/src/components/ExpenseDashboardPage.js
+++ router/src/components/ExpenseDashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilter';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
         <ExpenseListFilters />
         <ExpenseList />
+        <ExpensesSummary />
     </div>
 );
 
