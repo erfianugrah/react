d63ad78a22ad8c13aaf4f1cbde95ccc33efde727
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-07-01 09:49:20 +0800

    Asynchronous Redux Actions.1

--- router/src/components/AddExpensePage.js
+++ router/src/components/AddExpensePage.js
@@ -2,25 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { startAddExpense } from '../actions/expenses';
-import { render } from 'enzyme';
 
 export class AddExpensePage extends React.Component {
     onSubmit = (expense) => {
-                // props.dispatch(addExpense(expense));
-            this.props.startAddExpense(expense);
-            this.props.history.push('/');
+    this.props.startAddExpense(expense);
+    this.props.history.push('/');
     };
     render() {
-        return (
-            <div>
-                <h1>Add Expense</h1>
-                <ExpenseForm 
-                    onSubmit={this.onSubmit}
-                />
-            </div>
-        );
-    };
-};
+    return (
+    <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+            onSubmit={this.onSubmit}
+        />
+    </div>
+    );
+    }
+}
 
 const mapDispatchToProps = (dispatch) => ({
     startAddExpense: (expense) => dispatch(startAddExpense(expense))
