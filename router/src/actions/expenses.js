b5ea92a2d96c27dea6ed73e2958adcdebbf6b67b
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-07-01 09:42:13 +0800

    Asynchronous Redux Actions

--- router/src/actions/expenses.js
+++ router/src/actions/expenses.js
@@ -1,23 +1,29 @@
 import {v4 as uuid} from 'uuid';
+import database from '../firebase/firebase';
 
 // ADD_EXPENSE
-export const addExpense = (
-    {
+export const addExpense = (expense) => ({
+    type: 'ADD_EXPENSE',
+    expense
+  });
+
+export const startAddExpense = ( expense = {} ) => {
+  return (dispatch) => {
+    const {
       description = '',
       note = '',
       amount = 0,
       createdAt = 0
-    } = {}
-  ) => ({
-    type: 'ADD_EXPENSE',
-    expense: {
-      id: uuid(),
-      description,
-      note,
-      amount,
-      createdAt
-    }
-  });
+    }  = expenseData;
+    const expense = { description, note, amount, createdAt };
+      database.ref('expenses').push(expense).then((ref) => {
+        dispatch(addExpense({
+          id: ref.key,
+          ...expense
+        }));
+      });
+  };
+};
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
--- router/src/firebase/firebase.js
+++ router/src/firebase/firebase.js
@@ -15,6 +15,8 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
+export { firebase, database as default };
+
 // database.ref('expenses')
 //     .once('value')
 //     .then((snapshot) => {
