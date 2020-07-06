b4850fb57e7041a706c7c28f53e86d229f037074
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-07-06 12:59:42 +0800

    fetching expenses.2

--- router/src/app.js
+++ router/src/app.js
@@ -19,5 +19,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
- 
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
