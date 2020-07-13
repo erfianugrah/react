ca6c4c3177f54a0f8fa992df26d640f2c7a026c7
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-07-13 09:44:23 +0800

    fixing imports

--- router/src/routers/AppRouter.js
+++ router/src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
@@ -9,7 +9,7 @@ import NotFoundPage from '../components/NotFoundPage';
 import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history}>
