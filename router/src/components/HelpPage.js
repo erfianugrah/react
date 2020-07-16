988f4b6995872c6326a229aa0b3f1ce6c13efc3a
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-07-16 09:03:50 +0800

    Public Routes

--- router/src/components/HelpPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelpPage = () => (
-    <div>
-        <h1>This is from my help page component</h1>
-    </div>
-);
-
-export default HelpPage;
--- router/src/routers/AppRouter.js
+++ router/src/routers/AppRouter.js
@@ -5,9 +5,9 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import NotFoundPage from '../components/NotFoundPage';
-import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute.js';
+import PublicRoute from './PublicRoute';
 
 export const history = createBrowserHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-            <Route path="/" component={ LoginPage } exact={true} />
+            <PublicRoute path="/" component={ LoginPage } exact={true} />
             <PrivateRoute path="/dashboard" component={ ExpenseDashboardPage } />
             <PrivateRoute path="/create" component={AddExpensePage} />
             <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
             <Route component={NotFoundPage} />
         </Switch>
         </div>
