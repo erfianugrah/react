68fffabf7ee56716d2c92c1d325744eee1845b19
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-07-21 09:11:57 +0800

    Styling Inputs

--- router/src/components/ExpenseListFilter.js
+++ router/src/components/ExpenseListFilter.js
@@ -34,14 +34,20 @@ export class ExpenseListFilters extends React.Component {
         <div className="content-container">
             <div className="input-group">
                 <div className="input-group__item">
-                    <input  type="text" 
+                    <input  
+                        type="text"
+                        className="text-input"
+                        placeholder="Search Expenses" 
                         value={this.props.filters.text} 
                         onChange={this.onTextChange} 
                     />
                 </div>
                 <div className="input-group__item">
-                    <select value={this.props.filters.sortBy} 
-                        onChange={this.onSortChange} >
+                    <select 
+                        className="select"
+                        value={this.props.filters.sortBy} 
+                        onChange={this.onSortChange}
+                        >
                         <option value="date">Date</option>
                         <option value="amount">Amount</option>
                     </select>
