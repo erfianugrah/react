9a51c8377213bf0cec63517481568cd7a1dc80f8
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-07-23 09:30:09 +0800

    Wrangler edits

--- indecision/webpack.config.js
+++ indecision/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             ]
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
